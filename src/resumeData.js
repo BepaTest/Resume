2732497dc3f64763cc132054fef139f7b05c7ba4
Vera Testard <vera.testard@gmail.com>
2019-09-02 22:15:27 +0200

    array to finish

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Vera Testard",
+  "role": "Développeuse Frontend",
+  "linkedinId":"Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": " ",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/veratestard/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/BepaTest",
+        "className":"fa fa-github"
+      }
+    ],
+  "aboutme":"Forte de plusieurs expériences dans l’industrie en France et à l'étranger, je viens de terminer une formation de développeuse front-end React JS pour enfin travailler dans l'univers de l'Informatique.",
+  "address":"Lyon / Condrieu",
+  "website":"",
+  "education":[
+    {
+      "UniversityName":"Mumbai University",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2018",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"Some University",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Wild Code School - Lyon",
+      "specialization":"Développeuse JS React en formation",
+      "MonthOfArriving":"02",
+      "YearOfArriving": "2019",
+      "MonthOfLeaving":"06",
+      "YearOfLeaving":"2019",
+      Achievements: ["Formation basée sur la pratique. Réalisation d'un site web en équipe de 5 personnes, conception à l'aide d'une maquette / wireframe, utilisation de HTML, CSS, JavaScript, Bootstrap, mise en pratique de Git. https://bepatest.github.io/Yoshi-save-the-baby/index.html", 
+                      "Création d'un jeu vidéo en Javascript React sur l'univers des Pokemons, conception à l'aide d'une maquette / wireframe, gestion du projet en méthode Scrum en équipe de 6 personnes. https://pokeymon.netlify.com",
+                      "Création d'une application mobile permettant de mettre à disposition des services publics gratuitement en échange des actions bénévoles, équipe de 5 pesonnes, vainqueur de hackathon de 48h. https://unicity.netlify.com", 
+                      "Réalisation d'un site web responsive en équipe de 5 personnes pour la société Ido-Data, utilisation React Leaflet, geolocalisation"] 
+    },
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
